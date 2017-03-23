4aabddc1099fd2aca78e4ed2997d2273f0f946b0
matthewglassman <matthewglassmn78@gmail.com>
2017-03-23 14:58:55 -0400

    Changed filename from author.js to user.js

---
+++ models/user.js
@@ -0,0 +1,28 @@
+module.exports = function(sequelize, Datatypes){
+	var users = sequelize.define("users",{
+		user_id:{
+			//make unique key
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement:true
+		},
+		user_name:{
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		user_zip:{
+			type: DataTypes.INTEGER(5),
+			allowNull: false
+			validate: {
+				isNumeric: true,
+				isLengthFive: function(value){
+					var isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(value);
+				}
+
+			},
+		}
+		
+	});
+	return users;
+};
