fc8393f604aabc4173ad4d2db1ed76b20a219918
matthewglassman <matthewglassmn78@gmail.com>
2017-03-25 12:20:24 -0400

    Partially done users-api route

--- routes/users-api.js
+++ routes/users-api.js
@@ -1,1 +1,24 @@
 var db = require("../models");
+
+module.exports = function(app) {
+	app.get("/api/users", function(req, res){
+		db.users.findAll({}).then(function(dbusers){
+			res.json(dbusers);
+		});
+	});
+	app.post("/api/users", function(req, res){
+		console.log(req.body);
+		db.users.create(req.body).then(function(dbusers){
+			res.json(dbusers);
+		});
+	});
+	app.delete("/api/users/:id", function(req, res){
+		db.users.destroy({
+			where: {
+				id: req.params.users_id
+			}
+		}).then(function(dbusers) {
+			res.json(dbAuthor);
+		});
+	});
+};
