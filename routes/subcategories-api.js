4f9b544c2a9031f643da5852a1ab6d39f564de5c
matthewglassman <matthewglassmn78@gmail.com>
2017-03-26 10:52:06 -0400

    Added preliminary routing for Main and Sub Categories

--- routes/subcategories-api.js
+++ routes/subcategories-api.js
@@ -1,1 +1,19 @@
 var db = require("../models");
+
+module.exports = function(app) {
+	app.get("/api/subcategories", function(req, res){
+		db.subcategories.findAll({}).then(function(dbsubcategories){
+			res.json(dbsubcategories);
+		});
+	});
+	app.get("/api/subcategories/:id", function(req, res){
+		db.subcategories.findOne({
+			where: {
+				id: req.params.subcategories_id
+			}
+		}).then(function(dbsubcategories){
+			console.log(dbsubcategories);
+			res.json(dbsubcategories);
+		});
+	});
+};
