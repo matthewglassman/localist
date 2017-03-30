f2521e579c25023fc6a60fb7f6d901b255aeaeaa
MelissaLoggins <melissaloggins@gmail.com>
2017-03-30 18:01:14 -0400

    got the search going where it needs to go

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -33,12 +33,16 @@ module.exports = function(app) {
 
   // buyer route loads buyer
   app.get("/buyer", function(req, res) {
-    res.render("buyer");
+    res.sendFile(path.join(__dirname, "../public/search.html"));
+    // res.render("buyer");
   });
 
   // user profile route loads user profile ** maybe not be using this page **
+  // app.get("/profile", isAuthenticated, function(req, res) {
+  //   res.render("profile")
+  // });
+
   app.get("/profile", isAuthenticated, function(req, res) {
-    res.render("profile")
+    res.render("profile", { username: req.session.username })
   });
-  
 };
